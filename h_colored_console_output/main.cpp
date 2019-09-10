a916890671084cfa2d548408d2ce86d1fe87edfb
ger911man@gmail.com <ger911man@gmail.com>
2019-09-10 19:14:56 -0400

    nothing

--- h_colored_console_output/main.cpp
+++ h_colored_console_output/main.cpp
@@ -7,7 +7,7 @@ using namespace colorPrint;
 int main(){
     cout << "Color text example" << endl;
     cPrintln(FG_GREEN , BG_CYAN, TS_BOLD, "   -=[TS_BOLD GREEN on CYAN]=-   ");
-    cout << "Color text example" << endl;   cout << FGBLUE << BGYELLOW << TSUNDERLINE <<" -=[underlined blue on yellow]=- "<< NOCOLOR << endl;
+    cout << FGBLUE << BGYELLOW << TSUNDERLINE <<" -=[underlined blue on yellow]=- "<< NOCOLOR << endl;
     cout << "Done" << endl;
     return 0;
 }
