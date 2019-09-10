91fd5c7ba2ac621596e0ee04dfd38a6932ba4ea5
Gera <ger911man@gmail.com>
2019-09-10 15:53:14 -0400

    created clion project

---
+++ cmake-build-debug/CMakeFiles/cpp_help.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_help.dir/main.cpp.o"
+  "cpp_help.pdb"
+  "cpp_help"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_help.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
